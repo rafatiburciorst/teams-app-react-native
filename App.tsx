f8d813b5687036e1c86b70ea79bdd7612f156f80
Rafael Tiburcio <rafael.tiburcio@live.com>
2024-01-02 22:56:11 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { Loading } from "@components/Loading";
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
+import { Groups } from "@screens/Groups";
+import { NewGroup } from "@screens/NewGroup";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+
+import theme from "./src/theme";
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <NewGroup /> : <Loading />}
+    </ThemeProvider>
+  );
+}
